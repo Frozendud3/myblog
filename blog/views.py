0275b15fd4f9c95a0a7dfea40afc40f48011a555
Luis Carbogniani <luis.carbogniani@gmail.com>
2023-07-02 22:24:00 +0200

    primer commit

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+from .models import Post
+
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from django.contrib.auth import login, logout
+
+from django.shortcuts import render, redirect
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
