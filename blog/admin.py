d371ee4e317c23d385fcee803c48968e964e4145
Luis Carbogniani <luis.carbogniani@gmail.com>
2023-07-10 00:00:07 +0200

    6to commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
+from . import models
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'status', 'slug', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(models.Post)
+class Authoradmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'status', 'slug', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "name", "email", "publish", "status")
+    list_filter = ("status", "publish")
+    search_fields = ("name", "email", "content")
+
+admin.site.register(models.Category)    
+
 
 # Register your models here.
 # ID: luiscarbogniani pass: probando123
