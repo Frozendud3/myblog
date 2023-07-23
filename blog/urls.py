6062ae7be988355e5a631240278eeaa52d29064c
Luis Carbogniani <luis.carbogniani@gmail.com>
2023-07-23 10:03:50 +0200

    decimo commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (BlogHomePageView)
+from .views import (BlogHomePageView,PostDetailView)
 
 app_name='blog'
 
 urlpatterns = [
-    path('', BlogHomePageView.as_view(), name='home')
+    path('', BlogHomePageView.as_view(), name='home'),
+    path('slug:<slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
